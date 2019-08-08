6a382832332698641d4cb2809af347692b5fc813
Oves Shaikh <30339311+Oves786@users.noreply.github.com>
2019-08-08 19:42:10 +0530

    Update and rename omni_a6lte.mk to omni_j7xelte.mk

--- omni_j7xelte.mk
+++ omni_j7xelte.mk
@@ -1,5 +1,5 @@
 # Release name
-PRODUCT_RELEASE_NAME := Galaxy A6
+PRODUCT_RELEASE_NAME := Galaxy J7-2016
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
@@ -12,8 +12,8 @@ PRODUCT_PACKAGES += \
 	charger
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_a6lte
-PRODUCT_DEVICE := a6lte
+PRODUCT_NAME := omni_j7xelte
+PRODUCT_DEVICE := j7xelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SM-A600F
+PRODUCT_MODEL := SM-J710F
